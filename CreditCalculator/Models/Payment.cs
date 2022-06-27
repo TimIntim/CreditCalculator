273c37b42a39869c37d2ea658804d3452ba877a2
TimIntim <timintim1995@gmail.com>
2022-06-27 23:17:04 +0500

    Поправил вьюшку и создал модельку платежей

---
+++ CreditCalculator/Models/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreditCalculator.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PaymentDate { get; set; }
+        public decimal BodySum { get; set; }
+        public decimal MarginSum { get; set; }
+        public decimal BodyDebt { get; set; }
+
+        public int CreditCalculation { get; set; }
+
+        public virtual List<CreditCalculation> CreditCalculations { get; set; }
+    }
+}
