e2eb72ea0cbe52af92f7e13997e4448ec49c8334
TimIntim <timintim1995@gmail.com>
2022-06-28 22:45:42 +0500

    тест

--- CreditCalculator/Migrations/20220628174527_test.Designer.cs
+++ CreditCalculator/Migrations/20220628174527_test.Designer.cs
@@ -4,6 +4,7 @@ using CreditCalculator.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CreditCalculator.Migrations
 {
     [DbContext(typeof(CreditCalculatorContext))]
-    partial class CreditCalculatorContextModelSnapshot : ModelSnapshot
+    [Migration("20220628174527_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CreditCalculator/Migrations/20220628174527_test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreditCalculator.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BodyDebt",
+                table: "Payment",
+                newName: "DebtBalance");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DebtBalance",
+                table: "Payment",
+                newName: "BodyDebt");
+        }
+    }
+}
